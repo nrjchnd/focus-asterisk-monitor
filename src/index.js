98e79a58365ef562a30b56a8eef36112b1499930
Gustavo Moura <gustavomoura5555@outlook.com>
2021-01-26 18:18:30 -0300

    Queue Changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-const { response } = require('express');
 const express = require('express');
-const routes = require('./routes');
+const http = require('http');
+const socketio = require('socket.io');
 const cors = require('cors');
+const AMI = require('./ami/ami');
+
+const { getQueueStatus } = require('./helper');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(routes);
 
-app.listen(3333);
+const server = http.createServer(app);
+const io = new socketio.Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on('connection', socket => {
+    AMI.on('managerevent', event => {
+        switch (event.event) {
+            case 'QueueCallerJoin':
+            case 'Join':
+                getQueueStatus().then(response => {
+                    socket.emit('queue-status', {
+                        queue: response
+                    })
+                }).catch(err => console.log(err));
+                break;
+            case 'Leave':
+            case 'QueueCallerLeave':
+                getQueueStatus().then(response => {
+                    socket.emit('queue-status', {
+                        queue: response
+                    })
+                }).catch(err => console.log(err));
+                break;
+        }
+    })
+});
+
+server.listen(3334, '192.168.7.127');
--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { getQueue } = require('./controllers/QueueController');
-
-const routes = Router();
-
-routes.get('/queue', getQueue);
-
-module.exports = routes;
